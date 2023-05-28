2fa31d5f7ac6f166fe8402cc4a68069edea81d44
riad40 <riadabdelaziz40@gmail.com>
2023-05-28 13:33:33 +0100

    updated uploadImage destination

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,31 +1,37 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const multer_1 = __importDefault(require("multer"));
-const path_1 = __importDefault(require("path"));
-const today_1 = __importDefault(require("../helpers/today"));
+"use strict"
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod }
+    }
+Object.defineProperty(exports, "__esModule", { value: true })
+const multer_1 = __importDefault(require("multer"))
+const path_1 = __importDefault(require("path"))
+const today_1 = __importDefault(require("../helpers/today"))
 const storeImage = multer_1.default.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "dist/public/images");
+        cb(null, "public/images")
     },
     filename: (req, file, cb) => {
-        cb(null, (0, today_1.default)() + path_1.default.extname(file.originalname));
+        cb(
+            null,
+            (0, today_1.default)() + path_1.default.extname(file.originalname)
+        )
     },
-});
+})
 const uploadImage = (0, multer_1.default)({
     storage: storeImage,
     limits: {
         fileSize: 1024 * 1024 * 5,
     },
     fileFilter: (req, file, cb) => {
-        const fileTypes = /jpeg|jpg|png/;
-        const extName = fileTypes.test(path_1.default.extname(file.originalname).toLowerCase());
-        const mimeType = fileTypes.test(file.mimetype);
-        if (extName && mimeType)
-            return cb(null, true);
-        cb(new Error(JSON.stringify("file uploded is invalid")));
+        const fileTypes = /jpeg|jpg|png/
+        const extName = fileTypes.test(
+            path_1.default.extname(file.originalname).toLowerCase()
+        )
+        const mimeType = fileTypes.test(file.mimetype)
+        if (extName && mimeType) return cb(null, true)
+        cb(new Error(JSON.stringify("file uploded is invalid")))
     },
-});
-exports.default = uploadImage;
+})
+exports.default = uploadImage
